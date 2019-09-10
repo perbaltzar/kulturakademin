fa5e2ce124c45e304697ddf7a18381746620d117
Per Baltzar <per.baltzar@gmail.com>
2019-09-10 15:38:04 +0200

    added mock-tags to data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import playlists from './data/playlists.json';
+import youtube from './data/youtube.json';
+import tracks from './data/tracks.json';
 
 function App() {
+  console.log('playlist: ', playlists);
+  console.log('youtube: ', youtube);
+  console.log('tracks: ', tracks);
   return (
     <div className="App">
       <header className="App-header">
