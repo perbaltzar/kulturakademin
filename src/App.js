d4ad623835f58f07a33347b5f4d3a3db7055e558
Per Baltzar <per.baltzar@gmail.com>
2019-09-11 11:01:04 +0200

    added type to data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import playlists from './data/playlists.json';
 import youtube from './data/youtube.json';
 import tracks from './data/tracks.json';
 
-function App() {
-  console.log('playlist: ', playlists);
-  console.log('youtube: ', youtube);
-  console.log('tracks: ', tracks);
+const data = [playlists, youtube, tracks].flat();
+
+const App = () => {
+  const [searchIsOpen, setSearchIsOpen] = useState(false);
+  const [page, setPage] = useState('single-video');
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {searchIsOpen && <Search />}
+      {menuIsOpen && <Menu />}
+      {page === 'single-video' && <VideoPage id={} description={} />}
+      <Nav /> */}
     </div>
   );
 }
