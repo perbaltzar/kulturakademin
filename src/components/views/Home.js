15c7bb9451b113abfc702cb9182735bd7c0b34e2
Per Baltzar <per.baltzar@gmail.com>
2019-09-13 12:27:48 +0200

    Home View added

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,10 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-
+import React from 'react';
+import styled from 'styled-components';
+import HomeBanner from '../HomeBanner';
+import ContinueWatch from '../ContinueWatch';
 const StyledHome = styled.div``;
 
 const Home = props => {
-  return <StyledHome></StyledHome>;
+  return (
+    <StyledHome>
+      <HomeBanner />
+      <ContinueWatch />
+      {/* <News />
+      <Recommendations />
+      <ContinueListen />
+      <RecommendationVideo />
+      <RecommendationsPods /> */}
+    </StyledHome>
+  );
 };
 
 export default Home;
