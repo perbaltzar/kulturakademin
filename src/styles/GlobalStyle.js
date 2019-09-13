99753d52348ff76c1869df7ff3aa8dcfd5b79254
Per Baltzar <per.baltzar@gmail.com>
2019-09-13 12:27:39 +0200

    update theme

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
 *,*::before,*::after{
     padding: 0;
     margin:0;
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,7 +1,10 @@
 const Theme = {
-    colorDark: "#000",
-    colorLight: "#FFF",
-    colorPrimary: "tomato",
-    colorSecondary: "papyawhip"
-}
+  colorDark: '#000',
+  colorLight: '#FFF',
+  colorPrimary: 'tomato',
+  colorSecondary: 'papyawhip',
+  orange: '#E29829',
+  mobileSubHeadline: '',
+  font: 'Roboto',
+};
 export default Theme;
