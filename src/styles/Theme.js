a6e2ae841f714e718d21c4e3ff9ca36d23f15d8e
Adrian Jungnelius <adrianjung@Adrians-MacBook-Air.local>
2019-09-13 14:20:50 +0200

    added fonts to theme

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,7 +1,15 @@
 const Theme = {
+  // Color
   colorDark: "#323232",
   colorLight: "#FFF",
   colorPrimary: "tomato",
-  colorSecondary: "papyawhip"
+  colorSecondary: "papyawhip",
+
+  // Font
+  fontMobileH1: "36px/100% 'Roboto', sans-serif",
+  fontMobileH2: "24px/100% 'Roboto', sans-serif",
+  fontMobileH3: "20px/100% 'Roboto', sans-serif",
+  fontMobileH4: "16px/100% 'Roboto', sans-serif",
+  fontMobileP: "14px/100% 'Roboto', sans-serif"
 };
 export default Theme;
