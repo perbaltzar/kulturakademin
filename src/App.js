291e3c1449d228d6878fc0ddd6ee1cc6f080db84
Adrian Jungnelius <adrianjung@Adrians-MacBook-Air.local>
2019-09-13 14:22:54 +0200

    added fraction to fix globalstyle bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyle";
+import GlobalStyles from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import Theme from "./styles/Theme";
 import Home from "./components/views/Home";
@@ -15,26 +15,28 @@ import Menu from "./components/modals/Menu";
 import { MenuContext } from "./components/MenuContext";
 
 const App = () => {
-  const [displayMenu, setDisplayMenu] = useState("none");
+  const [displayMenu, setDisplayMenu] = useState("block");
   return (
     <div className="App">
-      <GlobalStyle />
       <ThemeProvider theme={Theme}>
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/installningar" exact component={Settings} />
-            <Route path="/favoriter" exact component={Favourites} />
-            <Route path="/video/:id" component={VideoSingle} />
-            <Route path="/podd/:id" component={PoddSingle} />
-            <Route path="/kategori/:id" component={CategorySinglePage} />
-            <Route path="/" component={NotFound} />
-          </Switch>
-          <MenuContext.Provider value={{ displayMenu, setDisplayMenu }}>
-            <Menu />
-            <Nav />
-          </MenuContext.Provider>
-        </Router>
+        <>
+          <GlobalStyles />
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/installningar" exact component={Settings} />
+              <Route path="/favoriter" exact component={Favourites} />
+              <Route path="/video/:id" component={VideoSingle} />
+              <Route path="/podd/:id" component={PoddSingle} />
+              <Route path="/kategori/:id" component={CategorySinglePage} />
+              <Route path="/" component={NotFound} />
+            </Switch>
+            <MenuContext.Provider value={{ displayMenu, setDisplayMenu }}>
+              <Menu />
+              <Nav />
+            </MenuContext.Provider>
+          </Router>
+        </>
       </ThemeProvider>
     </div>
   );
