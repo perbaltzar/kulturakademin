035fc4a0415177ba5d13b331f3d8f93325763ffe
Per Baltzar <per.baltzar@gmail.com>
2019-09-15 19:39:40 +0200

    Category Single Page component updated

--- src/components/CategorySinglePage.js
+++ src/components/CategorySinglePage.js
@@ -2,20 +2,37 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import capitalize from '../lib/capitalize';
+
 import CategoryBanner from './CategoryBanner';
+import Video from './Video';
 
 const StyledCategorySinglePage = styled.div`
   background-color: ${props => props.theme.colorDark};
+  min-height: 100%;
 `;
 
 const CategorySingle = ({match}) => {
   const [category, setCategory] = useState('');
-    useEffect(() => {
-        setCategory(match.params.id);
-    }, [match])
+
+
+  useEffect(() => {
+      setCategory(match.params.id);
+ 
+  }, [match])
+  
   return (
     <StyledCategorySinglePage>
-      <CategoryBanner text={capitalize(category)} />
+    
+    <CategoryBanner text={capitalize(category)} />
+    <Video 
+      title={'Video Title'} 
+      description={'This is the description of the clip'}
+    />
+    <Video 
+      title={'Video Title'} 
+      description={'This is the description of the clip'}
+    />
+
     </StyledCategorySinglePage>
   );
 };
