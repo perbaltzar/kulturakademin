a197b58fdd696bdbbb6aeea17a83e79d4e7e0f69
Per Baltzar <per.baltzar@gmail.com>
2019-09-15 20:31:43 +0200

    put real data in category single page

--- src/components/CategorySinglePage.js
+++ src/components/CategorySinglePage.js
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import capitalize from '../lib/capitalize';
-
+import findMediaByCategory from '../lib/search/findMediaByCategory';
 import CategoryBanner from './CategoryBanner';
 import Video from './Video';
+import Pod from './Pod';
+
+import youtube from '../data/youtube.json';
+import tracks from '../data/tracks.json';
+import playlists from '../data/playlists.json';
+
+let data = [youtube, tracks, playlists].flat();
+data = findMediaByCategory('musik', data);
 
 const StyledCategorySinglePage = styled.div`
   background-color: ${props => props.theme.colorDark};
@@ -13,25 +21,52 @@ const StyledCategorySinglePage = styled.div`
 
 const CategorySingle = ({match}) => {
   const [category, setCategory] = useState('');
-
+  const [media] = useState(data);
+  
 
   useEffect(() => {
       setCategory(match.params.id);
- 
+
+
   }, [match])
   
   return (
     <StyledCategorySinglePage>
     
     <CategoryBanner text={capitalize(category)} />
-    <Video 
-      title={'Video Title'} 
-      description={'This is the description of the clip'}
-    />
-    <Video 
-      title={'Video Title'} 
-      description={'This is the description of the clip'}
-    />
+    {media.map((media, i) => {
+      if (media.type === 'video'){
+        return (
+          <Video 
+            key={i}
+            title={media.title}
+            description={media.description && `${media.description.substr(0, 100)}...`}
+            thumbnail={media.thumbnail}
+            saved={false}
+          />
+        )
+      }else if(media.type === 'podcast'){
+        return (
+          <Pod 
+            key={i}
+            title={media.title}
+            description={media.description && `${media.description.substr(0, 100)}...`}
+            thumbnail={media.thumbnail}
+            saved={false}
+          />
+          )
+        }else if (media.type === 'playlist'){
+          return (
+          <Pod 
+            key={i}
+            title={media.title}
+            description={media.description && `${media.description.substr(0, 100)}...`}
+            thumbnail={media.thumbnail}
+            saved={false}
+          />)
+        }
+      })
+    }
 
     </StyledCategorySinglePage>
   );
