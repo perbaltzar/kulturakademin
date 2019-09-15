9fa6328aac6420b161e388c96564d892b5d7a788
Per Baltzar <per.baltzar@gmail.com>
2019-09-15 15:41:27 +0200

    banner in category single

--- src/components/CategorySinglePage.js
+++ src/components/CategorySinglePage.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const StyledCategorySinglePage = styled.div``;
+import capitalize from '../lib/capitalize';
+import CategoryBanner from './CategoryBanner';
 
-const CategorySingle = props => {
-  return <StyledCategorySinglePage></StyledCategorySinglePage>;
+const StyledCategorySinglePage = styled.div`
+  background-color: ${props => props.theme.colorDark};
+`;
+
+const CategorySingle = ({match}) => {
+  const [category, setCategory] = useState('');
+    useEffect(() => {
+        setCategory(match.params.id);
+    }, [match])
+  return (
+    <StyledCategorySinglePage>
+      <CategoryBanner text={capitalize(category)} />
+    </StyledCategorySinglePage>
+  );
 };
 
 export default CategorySingle;
