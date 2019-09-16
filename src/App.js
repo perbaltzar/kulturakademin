343b33e729e056399bacb20cac7ec49aebe2ca66
Per Baltzar <per.baltzar@gmail.com>
2019-09-16 11:26:26 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Theme from "./styles/Theme";
 import Home from "./components/views/Home";
 import Nav from "./components/modals/Nav";
 import VideoSingle from "./components/VideoSingle";
+import VideoPlayer from './components/modals/VideoPlayer';
+import PodPlayer from './components/modals/PodPlayer';
 import PoddSingle from "./components/PoddSingle";
 import CategorySinglePage from "./components/CategorySinglePage";
 import Settings from "./components/views/Settings";
@@ -13,14 +15,19 @@ import Favourites from "./components/views/Favourites";
 import NotFound from "./components/views/NotFound";
 import Menu from "./components/modals/Menu";
 import { MenuContext } from "./components/MenuContext";
+import { PlayerContext } from "./components/PlayerContext";
+
 const StyledApp = styled.div`
   height: 100vh;
+  background-color: ${props => props.theme.colorDark};
 `;
 const App = () => {
   const [displayMenu, setDisplayMenu] = useState("none");
+  const [playerVisible, setPlayerVisible] = useState(false);
+
   return (
-    <div className="App">
-      <StyledApp>
+   
+      <StyledApp className="App">
         <ThemeProvider theme={Theme}>
           <>
             <GlobalStyles />
@@ -29,11 +36,17 @@ const App = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/installningar" exact component={Settings} />
                 <Route path="/favoriter" exact component={Favourites} />
-                <Route path="/video/:id" component={VideoSingle} />
+                <PlayerContext.Provider value ={{ playerVisible, setPlayerVisible }} >
+                  <Route path="/video/:id" component={VideoSingle} />
+                </PlayerContext.Provider>
                 <Route path="/podd/:id" component={PoddSingle} />
                 <Route path="/kategori/:id" component={CategorySinglePage} />
                 <Route path="/" component={NotFound} />
               </Switch>
+              <PlayerContext.Provider value ={{ playerVisible, setPlayerVisible }} >
+                {playerVisible && <VideoPlayer />}
+                {/* <PodPlayer /> */}
+              </PlayerContext.Provider>
               <MenuContext.Provider value={{ displayMenu, setDisplayMenu }}>
                 <Menu />
                 <Nav />
@@ -42,7 +55,7 @@ const App = () => {
           </>
         </ThemeProvider>
       </StyledApp>
-    </div>
+  
   );
 };
 
