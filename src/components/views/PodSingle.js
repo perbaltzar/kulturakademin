e81b9ad610f0328f372ac3cb26baf8a133d0186e
Per Baltzar <per.baltzar@gmail.com>
2019-09-16 20:58:20 +0200

    set PodPlayer visible on PodsinglePage

--- src/components/views/PodSingle.js
+++ src/components/views/PodSingle.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import PodPlayer from '../players/PodPlayer';
+import { PlayerContext } from '../Context';
 const StyledPodSingle = styled.div``;
 
 const PodSingle = props => {
+  const { playerVisible, setPlayerVisible } = useContext(PlayerContext);
+  useEffect(() => {
+    setPlayerVisible(true);
+  });
   return (
     <StyledPodSingle>
       <PodPlayer />
