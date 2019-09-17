fb6d3ce4dbf570020f34791fa7a7d10d0b616f91
Per Baltzar <per.baltzar@gmail.com>
2019-09-17 16:06:43 +0200

    PodSingle styling

--- src/components/views/PodSingle.js
+++ src/components/views/PodSingle.js
@@ -1,21 +1,63 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { PlayerContext } from '../Context';
+import TagBox from '../miniature/TagBox';
+import tracks from '../../data/tracks.json';
+import selectTrackById from '../../lib/search/selectTrackById';
 
 const StyledPodSingle = styled.div`
   height: 100vh;
-  padding-bottom: 100px;
+  padding: 0 20px;
   overflow: scroll;
+  background: ${props => props.theme.colorDark};
+`;
+
+const StyledTagsContainer = styled.div`
+  display: flex;
+`;
+
+const StyledHero = styled.div`
+  padding: 80px 80px 0 80px;
+  margin-bottom: 30px;
+  img {
+    width: 100%;
+  }
+  h2 {
+    margin-top: 10px;
+    color: white;
+  }
 `;
 
 const PodSingle = ({ match }) => {
   const { setPlayerVisible, setMediaId } = useContext(PlayerContext);
+  const [pod, setPod] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
     setMediaId(match.params.id);
-  })
+    setPod(selectTrackById(match.params.id, tracks));
+    setLoaded(true);
+  }, [setMediaId, match.params.id]);
+
+  useEffect(() => {
+    if (loaded) console.log(pod);
+  }, [loaded, pod]);
+
   return (
     <StyledPodSingle>
-      <button onClick={() => setPlayerVisible('pod')}>PLAY</button>
+      {loaded && (
+        <>
+          <StyledHero>
+            <img onClick={() => setPlayerVisible('pod')} src={pod.thumbnail} alt="thumbnail" />
+            <h2>{pod.title.substr(5, 1000)}</h2>
+          </StyledHero>
+          <StyledTagsContainer>
+            {pod.tags.map(tag => {
+              return <TagBox text={tag} />;
+            })}
+          </StyledTagsContainer>
+        </>
+      )}
     </StyledPodSingle>
   );
 };
