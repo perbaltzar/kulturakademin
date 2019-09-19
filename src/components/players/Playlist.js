5f269fd94456be8c33e850327e9406a66b2dbac7
Per Baltzar <per.baltzar@gmail.com>
2019-09-19 14:58:39 +0200

    Playlist with real data

--- src/components/players/Playlist.js
+++ src/components/players/Playlist.js
@@ -2,39 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import PlaylistItem from './PlaylistItem';
 const StyledPlaylist = styled.div`
-    padding: 10px 20px;
-    background-color: white;
+  margin: 20px 0;
+  padding: 10px 20px;
+  background-color: white;
 `;
 
-const Playlist = props => {
-    return (
-        <StyledPlaylist>
-            <PlaylistItem 
-                number="1" 
-                img="https://i1.sndcdn.com/artworks-000557759277-nwur66-t500x500.jpg"
-                title="Eeva Bolin - Ny organisation för kultur i grundskola och förskola"
-                duration="370110"
-                plays="997"
-                playing={false}
-            />
-            <PlaylistItem 
-                number="2" 
-                img="https://i1.sndcdn.com/artworks-000557759277-nwur66-t500x500.jpg"
-                title="Eeva Bolin - Ny organisation för kultur i grundskola och förskola"
-                duration="370110"
-                plays="997"
-                playing={false}
-            />
-            <PlaylistItem 
-                number="3" 
-                img="https://i1.sndcdn.com/artworks-000557759277-nwur66-t500x500.jpg"
-                title="Eeva Bolin - Ny organisation för kultur i grundskola och förskola"
-                duration="370110"
-                plays="997"
-                playing={false}
-            />
-        </StyledPlaylist>
-    );
+const Playlist = ({ playlistTracks, number }) => {
+  return (
+    <StyledPlaylist>
+      {playlistTracks.map((track, i) => {
+        return (
+          <PlaylistItem
+            number={i + 1}
+            img={track.thumbnail}
+            title={track.title}
+            duration={track.duration}
+            plays="997"
+            playing={false}
+          />
+        );
+      })}
+    </StyledPlaylist>
+  );
 };
 
 export default Playlist;
