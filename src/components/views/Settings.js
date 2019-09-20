476033f8cfdbc77e416cf835578ef76e03d014cd
Per Baltzar <per.baltzar@gmail.com>
2019-09-20 10:33:49 +0200

    Settings Added

--- src/components/views/Settings.js
+++ src/components/views/Settings.js
@@ -1,7 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const StyledSettings = styled.div``;
+const StyledSettings = styled.div`
+  background-color: ${props => props.theme.colorDark};
+  padding-bottom: 80px;
+  height: 100vh;
+`;
 
 const Settings = props => {
   return <StyledSettings></StyledSettings>;
