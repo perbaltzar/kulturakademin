311c97440769855ded170f2dd320c3fb65249a95
Per Baltzar <per.baltzar@gmail.com>
2019-09-20 10:33:43 +0200

    Favourites View added

--- src/components/views/Favourites.js
+++ src/components/views/Favourites.js
@@ -1,7 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const StyledFavourites = styled.div``;
+const StyledFavourites = styled.div`
+  background-color: ${props => props.theme.colorDark};
+  padding-bottom: 80px;
+  height: 100vh;
+`;
 
 const Favourites = props => {
   return <StyledFavourites></StyledFavourites>;
