763c6fd428d592458c3555b644dbf5dc9f3ddb0c
Per Baltzar <per.baltzar@gmail.com>
2019-09-23 15:15:09 +0200

    update Search

--- src/components/modals/search/Search.js
+++ src/components/modals/search/Search.js
@@ -7,8 +7,8 @@ import SearchBar from './SearchBar';
 import FilterBar from './FilterBar';
 import Line from '../../players/Line';
 import CategoryGrid from '../../categories/CategoryGrid';
-import Pod from '../../miniature/Pod';
-import Video from '../../miniature/Video';
+import PodResults from './PodResults';
+import VideoResults from './VideoResults';
 import NoResults from './NoResults';
 import TopResults from './TopResults';
 import History from './History';
@@ -43,25 +43,22 @@ const Search = props => {
   const [results, setResults] = useState([]);
 
   const [chosen, setChosen] = useState('a-ö');
-
   const [showView, setShowView] = useState('suggestions');
 
   const { displaySearch } = useContext(SearchContext);
 
   const [searchQuery, setSearchQuery] = useState('');
   const handleChange = query => {
-    console.log(query);
     if (query.length > 0) {
       setSearchQuery(query);
       // All the results
       setResults(search(query));
 
-      setVideoResults(results.filter(result => result.type === 'video'));
-
-      setPodResults(results.filter(result => result.type === 'playlist'));
       if (results.length > 0) {
+        setVideoResults(results.filter(result => result.type === 'video'));
+        setPodResults(results.filter(result => result.type === 'playlist'));
         setShowView('results');
-        setTopResults([results[0], results[1]]);
+        setTopResults([results[0]]);
       } else {
         setShowView('no-results');
         setTopResults([]);
@@ -88,36 +85,8 @@ const Search = props => {
             <>
               <FilterBar chosen={chosen} onClick={chosenFilter => setChosen(chosenFilter)} />
               {topResults.length > 0 && <TopResults topResults={topResults} />}
-              <Line />
-
-              <p>Podd</p>
-              {podResults.length > 0 &&
-                podResults.map((pod, i) => {
-                  return (
-                    <Pod
-                      key={i}
-                      id={pod.id}
-                      thumbnail={pod.thumbnail}
-                      description={pod.description}
-                      title={pod.title}
-                    />
-                  );
-                })}
-              <Line />
-              <p>Video</p>
-
-              {videoResults.length > 0 &&
-                videoResults.map((video, i) => {
-                  return (
-                    <Video
-                      key={i}
-                      id={video.id}
-                      thumbnail={video.thumbnail}
-                      description={video.description}
-                      title={video.title}
-                    />
-                  );
-                })}
+              {podResults.length > 0 && <PodResults pods={podResults} />}
+              {videoResults.length > 0 && <VideoResults videos={videoResults} />}
               <p>Kategorier</p>
               <CategoryGrid gridTemplate="1fr 1fr 1fr" />
               <Line />
