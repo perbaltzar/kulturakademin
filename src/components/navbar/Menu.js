41d19158ec0386c65418bdeb5e798a633ac17626
Adrian Jungnelius <a.jungnelius@gmail.com>
2019-09-23 11:53:17 +0200

    added keyframes and updated design

--- src/components/navbar/Menu.js
+++ src/components/navbar/Menu.js
@@ -1,59 +1,102 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MenuContext } from '../Context';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import CrossIcon from './CrossIcon';
 import CategoryGrid from '../categories/CategoryGrid';
 
+// KeyFrames for menu
+const fadeIn = keyframes`
+  from {
+    transform: translateX(100vw)
+  }
+
+  to {
+    transform: translateY(0vw)
+  }
+`;
+const fadeOut = keyframes`
+  from {
+    transform: translateX(0vw)
+  }
+
+  to {
+    transform: translateX(100vw)
+  }
+`;
 const StyledMenu = styled.div`
-  position: fixed;
-  top: 0;
-  display: ${props => props.display};
   z-index: 1000;
   height: 100vh;
   width: 100%;
+  position: fixed;
+  top: 0;
+
+  animation: ${props =>
+    props.animation
+      ? css`
+          ${fadeOut} 0.3s ease-in-out forwards
+        `
+      : css`
+          ${fadeIn} 0.3s ease-in-out forwards
+        `};
+  display: ${props => props.display};
   overflow: auto;
-  padding: 0 70px;
+  padding: 8vh 70px;
   background-color: ${props => props.theme.colorDark};
   color: ${props => props.theme.colorLight};
-  h2 {
-    margin-bottom: 8px;
-  }
-  h4 {
-    margin-bottom: 65px;
-  }
+
   .menu-items {
-    margin-top: 100px;
-    h2 {
-      margin-bottom: 25px;
-    }
-    h4 {
-      margin-top: 50px;
-    }
-    span {
-      display: block;
-      top: -60px;
-      background-color: ${props => props.theme.colorLight};
-      margin: 5px 0px 25px 0px;
-      height: 2px;
-      width: 70px;
-    }
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+    grid-row-gap: 20px;
+    margin-bottom: 6vh;
   }
   .category-grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 10px;
-    margin-bottom: 57px;
+    margin-bottom: 4vh;
   }
 `;
-
+const StyledFlex = styled.div`
+  display: flex;
+  flex-direction: ${props => (props.column ? 'column' : 'row')};
+  img {
+    margin-left: 20px;
+  }
+  span {
+    background-color: ${props => props.theme.colorLight};
+    margin: 5px 0px 10px 0px;
+    height: 2px;
+    width: 70px;
+  }
+`;
 const Menu = props => {
-  const { displayMenu, setDisplayMenu } = useContext(MenuContext);
+  const { displayMenu, setDisplayMenu, toggleMenuAnimation, setToggleMenuAnimation } = useContext(
+    MenuContext,
+  );
+
+  const fadeOutAnim = () => {
+    setToggleMenuAnimation(!toggleMenuAnimation);
+    setTimeout(() => {
+      setDisplayMenu('none');
+    }, 300);
+  };
+
   return (
-    <StyledMenu display={displayMenu.toString()}>
-      <CrossIcon onClick={() => setDisplayMenu('none')} />
-      <div className="menu-items">
-        <div onClick={() => setDisplayMenu('none')}>
+    <StyledMenu {...props} display={displayMenu.toString()} animation={toggleMenuAnimation}>
+      <CrossIcon
+        onClick={() => {
+          fadeOutAnim();
+        }}
+      />
+      <div
+        onClick={() => {
+          fadeOutAnim();
+        }}
+      >
+        <div className="menu-items">
           <Link to="/">
             <h2>Hem</h2>
           </Link>
@@ -65,20 +108,23 @@ const Menu = props => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2>Kurser</h2>
+            <StyledFlex>
+              <h2>Kurskatalog</h2>
+              <img src="/assets/icons/Link-to.svg" alt=""></img>
+            </StyledFlex>
           </a>
-          <Link to="/installningar">
-            <h2>Inställningar</h2>
-          </Link>
-          <h2>English</h2>
-          <h4>Kategorier</h4>
         </div>
-        <span></span>
-        <CategoryGrid gridTemplate="1fr 1fr" onClick={() => setDisplayMenu('none')}></CategoryGrid>
+        <StyledFlex column>
+          <h5>Kategorier</h5>
+          <span></span>
+        </StyledFlex>
       </div>
+      <CategoryGrid gridTemplate="1fr 1fr" onClick={() => fadeOutAnim()}></CategoryGrid>
       <a href="https://www.kulturakademin.com/" target="_blank" rel="noopener noreferrer">
-        <h2>Tillbaka till</h2>
-        <h4>kulturakademin.com</h4>
+        <StyledFlex>
+          <h5>kulturakademin.se</h5>
+          <img src="/assets/icons/Link-to.svg" alt=""></img>
+        </StyledFlex>
       </a>
     </StyledMenu>
   );
