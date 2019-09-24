5e4d13559c954d06078f5ff9f271fcab61647af5
Per Baltzar <per.baltzar@gmail.com>
2019-09-24 20:20:39 +0200

    added progressbar

---
+++ src/components/players/ProgressBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledProgressBar = styled.div`
+  height: 4px;
+  width: ${props => props.progress}%;
+  background-color: ${props => props.theme.orange};
+`;
+
+const ProgressBar = ({ progress }) => {
+  return <StyledProgressBar progress={progress}></StyledProgressBar>;
+};
+
+export default ProgressBar;
