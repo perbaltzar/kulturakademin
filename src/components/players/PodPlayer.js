0a867343323bf376474539a749bbcd0bb52cf8cd
Per Baltzar <per.baltzar@gmail.com>
2019-09-25 09:33:47 +0200

    added +/- 15 buttons and current time in podplayer

--- src/components/players/PodPlayer.js
+++ src/components/players/PodPlayer.js
@@ -6,6 +6,7 @@ import tracks from '../../data/tracks.json';
 import { PlayerContext } from '../Context';
 
 import selectMediaById from '../../lib/search/selectMediaById';
+import displayProperTime from '../../lib/displayProperTimer';
 
 const clientId = '45ca7c7c9b41fdcb2501bb7dd27e168b';
 
@@ -15,14 +16,17 @@ const StyledCloseButton = styled.button``;
 
 const StyledReactPlayer = styled.div`
   position: fixed;
-  height: 60px;
+  height: 80px;
   width: 100%;
   background-color: #1c1c1c;
   bottom: 60px;
   z-index: 101;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
   /* CONTROLLER CONTAINER */
   div:first-of-type {
-    height: 90%;
+    height: 52px;
     width: 100%;
     display: flex;
     justify-content: space-around;
@@ -40,6 +44,12 @@ const StyledReactPlayer = styled.div`
     svg {
       fill: white;
     }
+  }
+  div:nth-child(2) {
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+    color: white;
   }
   div:last-of-type {
     /* PROGRESSBAR */
@@ -63,15 +73,23 @@ const Player = withSoundCloudAudio(props => {
   return (
     <StyledReactPlayer currentTime={currentTime} duration={duration}>
       <div>
-        <StyledMoveButton>-15</StyledMoveButton>
+        <StyledMoveButton>
+          <img src="/assets/icons/15back.svg" alt="-15" />
+        </StyledMoveButton>
         <PlayButton
           className="flex-none h4 button button-transparent button-grow rounded"
           {...props}
         />
-        <StyledMoveButton>+15</StyledMoveButton>
+        <StyledMoveButton>
+          <img src="/assets/icons/15forward.svg" alt="+15" />
+        </StyledMoveButton>
         <StyledCloseButton onClick={() => setPlayerVisible('none')}>
           <img src="/assets/icons/close.svg" alt="X" />
         </StyledCloseButton>
+      </div>
+      <div>
+        <p>{displayProperTime(currentTime * 1000)}</p>
+        <p>{displayProperTime(duration * 1000)}</p>
       </div>
       <Progress duration={track ? track.duration / 1000 : 0} currentTime={currentTime} {...props} />
     </StyledReactPlayer>
