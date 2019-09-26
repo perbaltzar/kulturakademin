3cc9005fd537d70c223bf22887fef5ea23d70f9d
Per Baltzar <per.baltzar@gmail.com>
2019-09-26 15:29:52 +0200

    Added mockdata in History

--- src/components/modals/search/History.js
+++ src/components/modals/search/History.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import Line from '../../players/Line';
-const StyledHistory = styled.div``;
+const StyledHistory = styled.div`
+  p {
+    margin: 25px 0;
+  }
+`;
 
 const History = props => {
   return (
     <StyledHistory>
       <h3>Sökhistorik</h3>
       <Line />
+      <p>Scenkonst</p>
+      <p>Trumpet</p>
+      <p>Hantverk</p>
     </StyledHistory>
   );
 };
