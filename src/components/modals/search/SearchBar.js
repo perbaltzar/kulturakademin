5019f32602027697ca1ccdbfca1ecc60ffad0271
Per Baltzar <per.baltzar@gmail.com>
2019-09-26 15:30:28 +0200

    added back arrow and close in search bar

--- src/components/modals/search/SearchBar.js
+++ src/components/modals/search/SearchBar.js
@@ -1,47 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import SearchOrClose from './SearchOrClose';
+// import { SearchContext } from '../../Context';
+
 const StyledSearchBar = styled.div`
   background-color: ${props => props.theme.orange};
   width: 100%;
   height: 66px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding: 0px 20px;
 
-  input {
-    height: 30px;
-    width: 80vw;
-    background-color: transparent;
-    border: none;
-    border-bottom: solid 2px white;
-    font-size: 16px;
-    color: white;
-    ::placeholder {
-      color: white;
-    }
+  img {
+    justify-self: flex-start;
   }
   form {
+    input {
+      height: 30px;
+      width: 80vw;
+      background-color: transparent;
+      border: none;
+      border-bottom: solid 2px white;
+      font-size: 16px;
+      color: white;
+      ::placeholder {
+        color: white;
+      }
+    }
     display: flex;
+    div {
+      right: 0;
+    }
   }
 `;
 
-const SearchBar = ({ handleChange }) => {
+const SearchBar = ({ handleChange, onFocus }) => {
   const [query, setQuery] = useState('');
+  const [magnifying, setMagnifying] = useState(true);
+  // const { setDisplaySearch } = useContext(SearchContext);
 
   return (
     <StyledSearchBar>
+      <img src="/assets/icons/go-back.svg" alt="back" />
       <form onSubmit={e => e.preventDefault()}>
         <input
           type="text"
           placeholder="Sök..."
           onChange={({ target }) => {
             setQuery(target.value);
+            if (target.value.length < 1) setMagnifying(true);
           }}
-          onKeyUp={() => handleChange(query)}
+          onKeyUp={() => {
+            handleChange(query);
+            if (query.length > 0) setMagnifying(false);
+          }}
           value={query}
+          onBlur={() => {
+            if (query.length < 1) setMagnifying(true);
+          }}
+          onFocus={onFocus}
         />
-        <SearchOrClose magnifying={true} />
+        <SearchOrClose magnifying={magnifying} onClick={() => setQuery('')} />
       </form>
     </StyledSearchBar>
   );
