75796c8519de4ad94e7db183e15d86be7e3fb8ab
Per Baltzar <per.baltzar@gmail.com>
2019-09-26 14:02:31 +0200

    Video and Pod in Suggestions

--- src/components/modals/search/Suggestions.js
+++ src/components/modals/search/Suggestions.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Line from '../../players/Line';
+import playlists from '../../../data/playlists.json';
+import youtube from '../../../data/youtube.json';
+import Pod from '../../miniature/Pod';
+import Video from '../../miniature/Video';
 
 const StyledSuggestions = styled.div``;
 
@@ -9,6 +13,20 @@ const Suggestions = props => {
     <StyledSuggestions>
       <h3>Förslag</h3>
       <Line />
+      <Video
+        title={youtube[3].title}
+        // description={media.description && `${media.description.substr(0, 70)}...`}
+        thumbnail={youtube[3].thumbnail}
+        saved={false}
+        id={youtube[3].id}
+      />
+      <Pod
+        title={playlists[7].title}
+        // description={media.description && `${media.description.substr(0, 70)}...`}
+        thumbnail={playlists[7].thumbnail}
+        saved={false}
+        id={playlists[7].id}
+      />
     </StyledSuggestions>
   );
 };
